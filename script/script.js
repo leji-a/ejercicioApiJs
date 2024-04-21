ff4591f939c342c57527f3998beab2826bd13808
Santiago Cézar <santiagocezar2013@gmail.com>
2024-04-21 17:34:25 -0300

    hola ale

--- script/script.js
+++ script/script.js
@@ -2,17 +2,8 @@ class ApiUserData{
     constructor(username){
         this.username = username
     }
-    displayData(){
+    displayData(data){
         if(this.username){
-            let data = {
-                id: this.id,
-                username: this.username,
-                name: this.name,
-                honor: this.honor,
-                clan: this.clan,
-                leaderboardPosition: this.leaderboardPosition,
-                skils: this.skills
-            }
             document.getElementById('dataDisplay').innerText = JSON.stringify(data, null, 2)
             document.getElementById('dataError').style.display = 'none'
         } else {
@@ -28,14 +19,7 @@ class ApiUserData{
         fetch('https://www.codewars.com/api/v1/users/' + this.username)
         .then(response => response.json())
         .then(data => {
-            this.id = data.id
-            this.username = data.username
-            this.name = data.name
-            this.honor = data.honor
-            this.clan = data.clan
-            this.leaderboardPosition = data.leaderboardPosition
-            this.skills = data.skills
-            this.displayData()
+            this.displayData(data)
         });
     }
 }
